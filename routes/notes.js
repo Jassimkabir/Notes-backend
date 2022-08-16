3acc7505d4e6f00982cf3ed418307518526b7f79
Jassimkabir <jassim@hamon.in>
2022-08-16 15:49:03 +0530

    Update error message

--- routes/notes.js
+++ routes/notes.js
@@ -27,6 +27,7 @@ router.get('/get-note/:id', async (req, res) => {
   try {
     const noteId = req.params.id;
     const userId = req.user.id;
+    console.log(userId);
 
     const enabled = optimizelyClient.isFeatureEnabled(
       'expand_or_update_note',
@@ -44,7 +45,9 @@ router.get('/get-note/:id', async (req, res) => {
 
       res.status(200).send(getNote);
     } else {
-      res.status(403).send('This feature is not available to you');
+      res
+        .status(400)
+        .send('This feature is either turned off or not available to you');
     }
   } catch (err) {
     console.error(err);
